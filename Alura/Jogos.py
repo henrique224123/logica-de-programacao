262c4b9231fd156ce4dca2cb1348cbdbbe7ec59a
Hollow <henrique.mororo23@gmail.com>
2021-12-16 18:46:34 -0300

    Logica

--- Alura/Jogos.py
+++ Alura/Jogos.py
@@ -1,4 +1,4 @@
-import Forca
+import forca
 import adivinhacao
 
 print("======================================")
@@ -11,7 +11,7 @@ jogo = int(input("Faça sua escolha: "))
 
 if(jogo == 1):
     print("Jogo Escolhiido: Forca")
-    Forca.Jogo2()
+    forca.Jogo2()
 elif(jogo == 2):
     print("Jogo Escolhido: Adivinhação")
     adivinhacao.Jogo1()
