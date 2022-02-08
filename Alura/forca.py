334eecf7772c619980cc38f33934f281420637f9
Hollow <henrique.mororo23@gmail.com>
2022-02-08 19:56:33 -0300

    Logica

--- Alura/forca.py
+++ Alura/forca.py
@@ -1,30 +1,51 @@
+import random
+
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))  # len devolve o tamanho da lista
+    palavra_secreta = palavras[numero].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
-    while(not enforcou and not acertou):
+    while not enforcou and not acertou:
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute.upper() == letra.upper():
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
 
         print(letras_acertadas)
 
     print("Fim do jogo")
 
-if(__name__ == "__main__"):
+
+if __name__ == "__main__":
     jogar()
